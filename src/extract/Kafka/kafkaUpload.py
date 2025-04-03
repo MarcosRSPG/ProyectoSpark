964883193487ce6a49f42cbc256aaf8dd8290d0a
MarcosRSPG <marcosrs.softwaredeveloper@gmail.com>
2025-04-03 02:32:22 +0200

    transformacion Completada

--- src/extract/Kafka/kafkaUpload.py
+++ src/extract/Kafka/kafkaUpload.py
@@ -2,9 +2,8 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import from_json
 from pyspark.sql.types import StructType, StringType, IntegerType, DoubleType
 
-aws_access_key_id='test'
-aws_secret_access_key='test'
-
+aws_access_key_id = 'test'
+aws_secret_access_key = 'test'
 
 def process_batch(batch_df, batch_id):
     print(f"Processing batch {batch_id} with {batch_df.count()} records.")
@@ -19,7 +18,7 @@ def process_batch(batch_df, batch_id):
             .option('fs.s3a.committer.name', 'partitioned') \
             .option('fs.s3a.committer.staging.conflict-mode', 'replace') \
             .option("fs.s3a.fast.upload.buffer", "bytebuffer")\
-            .mode('overwrite') \
+            .mode('append') \
             .json(path='s3a://data-lake/sales')
             
         except Exception as e:
@@ -30,61 +29,64 @@ def process_batch(batch_df, batch_id):
     else:
         print(f"Batch {batch_id} is empty.")
 
-spark = SparkSession.builder \
-    .appName("Streaming from Kafka") \
-    .config("spark.streaming.stopGracefullyOnShutdown", True) \
-    .config("spark.sql.shuffle.partitions", 4) \
-    .config("spark.hadoop.fs.s3a.endpoint", "http://localstack:4566") \
-    .config("spark.hadoop.fs.s3a.access.key", aws_access_key_id) \
-    .config("spark.hadoop.fs.s3a.secret.key", aws_secret_access_key) \
-    .config("spark.jars.packages","org.apache.spark:spark-hadoop-cloud_2.13:3.5.1,software.amazon.awssdk:s3:2.25.11,org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.1") \
-    .config("spark.hadoop.fs.s3a.path.style.access", "true") \
-    .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem") \
-    .config("spark.driver.extraClassPath", "/opt/spark/jars/s3-2.25.11.jar") \
-    .config("spark.executor.extraClassPath", "/opt/spark/jars/s3-2.25.11.jar") \
-    .master("spark://spark-master:7077") \
-    .getOrCreate()
+
+try:
+    spark = SparkSession.builder \
+        .appName("Streaming from Kafka") \
+        .config("spark.streaming.stopGracefullyOnShutdown", True) \
+        .config("spark.sql.shuffle.partitions", 4) \
+        .config("spark.hadoop.fs.s3a.endpoint", "http://localstack:4566") \
+        .config("spark.hadoop.fs.s3a.access.key", aws_access_key_id) \
+        .config("spark.hadoop.fs.s3a.secret.key", aws_secret_access_key) \
+        .config("spark.jars.packages", "org.apache.spark:spark-hadoop-cloud_2.13:3.5.1,software.amazon.awssdk:s3:2.25.11,org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.1") \
+        .config("spark.hadoop.fs.s3a.path.style.access", "true") \
+        .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem") \
+        .config("spark.driver.extraClassPath", "/opt/spark/jars/s3-2.25.11.jar") \
+        .config("spark.executor.extraClassPath", "/opt/spark/jars/s3-2.25.11.jar") \
+        .master("spark://spark-master:7077") \
+        .getOrCreate()
     
-    
-       
-df =spark  \
-  .readStream \
-  .format("kafka") \
-  .option("kafka.bootstrap.servers", "kafka:9093") \
-  .option("subscribe", "sales_stream") \
-  .load()
-  
+    spark.conf.set("spark.hadoop.fs.s3a.connection.maximum", "100")  # Optional: Tune connection pool
+    spark.conf.set("spark.hadoop.fs.s3a.fast.upload", "true")
+    spark.conf.set("spark.hadoop.fs.s3a.path.style.access", "true")  # Required if using path-style S3 URLs (older format)
+    spark.conf.set("spark.hadoop.fs.s3a.committer.name", "partitioned")  # Ensures partitioning for large datasets
 
-schema = StructType() \
-    .add("timestamp", IntegerType()) \
-    .add("store_id", IntegerType()) \
-    .add("product_id", StringType()) \
-    .add("quantity_sold", IntegerType()) \
-    .add("revenue", DoubleType())
+    df = spark \
+        .readStream \
+        .format("kafka") \
+        .option("kafka.bootstrap.servers", "kafka:9093") \
+        .option("subscribe", "sales_stream") \
+        .load()
 
-# Convert value column to JSON and apply schema
-df = df.selectExpr("CAST(value AS STRING)") \
-    .select(from_json("value", schema).alias("data")) \
-    .select("data.*")
+    schema = StructType() \
+        .add("timestamp", StringType()) \
+        .add("store_id", IntegerType()) \
+        .add("product_id", StringType()) \
+        .add("quantity_sold", IntegerType()) \
+        .add("revenue", DoubleType())
 
-# Print schema of DataFrame for debugging
-df.printSchema()
+    # Convertir la columna value a JSON y aplicar el esquema
+    df = df.selectExpr("CAST(value AS STRING)") \
+        .select(from_json("value", schema).alias("data")) \
+        .select("data.*")
 
-# Start the computation
-query = df \
-    .writeStream \
-    .outputMode("append") \
-    .format("console") \
-    .start()
+    # Imprimir el esquema del DataFrame para depuración
+    df.printSchema()
 
-query = df \
-    .writeStream \
-    .outputMode("append") \
-    .format("json") \
-    .option("path", "s3a://data-lake/sales") \
-    .option("checkpointLocation", "s3a://data-lake/checkopoint")\
-    .foreachBatch(process_batch) \
-    .option("header", "true")\
-    .start()
+    query = df.writeStream \
+        .format("json") \
+        .option("path", "s3a://data-lake/sales") \
+        .option("checkpointLocation", "s3a://data-lake/checkpoint")\
+        .trigger(processingTime="5 seconds")\
+        .foreachBatch(process_batch) \
+        .start()
 
-query.awaitTermination()
+    query.awaitTermination()
+
+except Exception as e:
+    print("Error reading data from Kafka:", e)    
+finally:
+    spark.stop()
+df = spark.read.json("s3a://data-lake/sales/")
+df.coalesce(1).write.mode("overwrite").json("s3a://data-lake/sales_compacted")
+
---
+++ src/transform/Kafka/kafkaProcessing.py
@@ -0,0 +1,71 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, when, mean, current_timestamp
+
+aws_access_key_id = 'test'
+aws_secret_access_key = 'test'
+
+timestamp= 'timestamp'
+stores= 'store_id'
+products= 'product_id'
+quantity= 'quantity_sold'
+revenue= 'revenue'
+tratado= 'Tratado'
+fecha_insercion= 'Fecha Insercion'
+
+spark = SparkSession.builder \
+    .appName("csvTransformData") \
+    .config("spark.hadoop.fs.s3a.endpoint", "http://localstack:4566") \
+    .config("spark.hadoop.fs.s3a.access.key", aws_access_key_id) \
+    .config("spark.hadoop.fs.s3a.secret.key", aws_secret_access_key) \
+    .config("spark.sql.shuffle.partitions", "4") \
+    .config("spark.jars.packages","org.apache.hadoop:hadoop-aws:3.3.4") \
+    .config("spark.hadoop.fs.s3a.path.style.access", "true") \
+    .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem") \
+    .master("spark://spark-master:7077") \
+    .getOrCreate()
+
+bucket_path = "s3a://data-lake/sales_compacted/part-00000-072c0a78-5846-428c-8dc5-7d929e919a8d-c000.json"
+df = spark.read.json(bucket_path)
+
+invalid_values = ["", "None", "STORE_ERROR", "PRODUCT_ERROR", "QUANTITY_ERROR", "REVENUE_ERROR", "TIMESTAMP_ERROR"]
+
+df_filtered = df.filter(~(df[timestamp].isin(invalid_values) | df[timestamp].isNull() | (df[timestamp] == 'None')))
+df_count = df_filtered.groupBy(timestamp).count()
+most_frequent_timestamp = df_count.orderBy(col('count').desc()).first()
+most_frequent_timestamp_value = most_frequent_timestamp[timestamp]
+
+quantity_mean = int(df.select(mean(col(quantity))).collect()[0][0])
+revenue_mean = df.select(mean(col(revenue))).collect()[0][0]
+
+df = df.withColumn(tratado, when(df[revenue].isin(invalid_values) | df[revenue].isNull() | (df[revenue] == 'None') | df[quantity].isin(invalid_values) | df[quantity].isNull() | (df[quantity] == 'None') | df[timestamp].isin(invalid_values) | df[timestamp].isNull() | (df[timestamp] == 'None'), True).otherwise(False))
+df = df.withColumn(timestamp, when(df[timestamp].isin(invalid_values) | df[timestamp].isNull() | (df[timestamp] == 'None'), most_frequent_timestamp_value).otherwise(df[timestamp]))
+
+# Filtra filas donde store_id o product_id estén en la lista de valores inválidos o sean nulos
+df = df.filter(~(df[stores].isin(invalid_values) | df[stores].isNull()))
+df = df.filter(~(df[products].isin(invalid_values) | df[products].isNull()))
+
+df = df.withColumn(quantity, when(df[quantity].isin(invalid_values) | df[quantity].isNull() | (df[quantity] == 'None'), quantity_mean).otherwise(df[quantity]))
+df = df.withColumn(revenue, when(df[revenue].isin(invalid_values) | df[revenue].isNull() | (df[revenue] == 'None'), revenue_mean).otherwise(df[revenue]))
+
+df = df.withColumn(fecha_insercion, current_timestamp())
+
+df = df.dropDuplicates()
+
+df = df.withColumn("timestamp", (col("timestamp") / 1000).cast("timestamp")) \
+       .withColumn(stores, col(stores).cast("int")) \
+       .withColumn(products, col(products).cast("string")) \
+       .withColumn(quantity, col(quantity).cast("int")) \
+       .withColumn(revenue, col(revenue).cast("double"))
+
+df.show()
+df.printSchema()
+
+df \
+    .write \
+    .format('csv') \
+    .option('header', 'true') \
+    .option('fs.s3a.committer.name', 'partitioned') \
+    .option('fs.s3a.committer.staging.conflict-mode', 'replace') \
+    .option("fs.s3a.fast.upload.buffer", "bytebuffer")\
+    .mode('overwrite') \
+    .csv(path='s3a://data-lake/kafka_processed', sep=',')
